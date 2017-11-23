b6c9fdf89a7997196d7d2dba2d04edc943ef9274
A-8032 <A-8032@10.161.138.48>
2017-11-23 16:45:25 +0530

    Added Message brocker

---
+++ src/main/java/com/mq/app/brocker/MessageBrocker.java
@@ -0,0 +1,19 @@
+package com.mq.app.brocker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.mq.app.bean.MessageBean;
+
+public class MessageBrocker {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MessageBrocker.class);
+
+	@Autowired
+    private JmsTemplate jmsTemplate;
+	public void send (MessageBean message) {
+		LOGGER.info("sending with convertAndSend() to queue <" + message + ">");
+		jmsTemplate.convertAndSend(message);		
+	}
+}
